fc4956b73a1eed7f1190bd07468b7f836d19216f
Aman Karmani <aman@tmm1.net>
2023-03-31 15:29:25 -0700

    call python3 here too

--- src/run.sh
+++ src/run.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 mkdir -p $DIR/data
 rm -rf $DIR/basic.xml
 
-python $DIR/ical_2_xmltv.py "https://calendar.google.com/calendar/ical/mg877fp19824mj30g497frm74o%40group.calendar.google.com/public/basic.ics" "TWiT.tv"
+python3 $DIR/ical_2_xmltv.py "https://calendar.google.com/calendar/ical/mg877fp19824mj30g497frm74o%40group.calendar.google.com/public/basic.ics" "TWiT.tv"
 
 mv $DIR/basic.xml $DIR/data/
